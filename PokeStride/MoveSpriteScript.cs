01da39f509a9b3ca128623aae03a315e9541f328
Sean A <seanseptember2002@gmail.com>
2022-12-02 18:23:56 -0500

    moving something

--- PokeStride/MoveSpriteScript.cs
+++ PokeStride/MoveSpriteScript.cs
@@ -6,31 +6,22 @@ using System.Threading.Tasks;
 using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
-using System.Runtime.CompilerServices;
-using Stride.Rendering.Sprites;
-using Stride.Graphics;
-using Stride.UI;
 using Stride.UI.Controls;
+using Stride.UI;
 
 namespace PokeStride
 {
-    public class MovementScript : SyncScript
+    public class MoveSpriteScript : SyncScript
     {
         // Declared public member fields and properties will show in the game studio
         bool moveable;
         float xVelocity, yVelocity;
-        ImageElement sprite;
-        Thickness border;
 
         public override void Start()
         {
             // Initialization of the script.
-            sprite = Entity.Get<UIComponent>().Page.RootElement.FindVisualChildOfType<ImageElement>("characterSprite");
-            Log.Info(sprite.ToString());
             xVelocity = 0.1f;
             yVelocity = 0.1f;
-            border = sprite.Margin;
-            moveable = true;
         }
 
         public override void Update()
@@ -40,26 +31,22 @@ namespace PokeStride
             {
                 if (Input.IsKeyDown(Keys.Left))
                 {
-                    Log.Info("Moving Left: " + border.Left);
-                    border.Left -= xVelocity;
-                    border.Right += xVelocity;
+                    this.Entity.Transform.Position.X -= xVelocity;
                 }
                 if (Input.IsKeyDown(Keys.Right))
                 {
-                    Log.Info("Moving Right: " + border.Left);
-                    border.Right -= xVelocity;
-                    border.Left += xVelocity;
+                    this.Entity.Transform.Position.X += xVelocity;
                 }
                 if (Input.IsKeyDown(Keys.Down))
                 {
-                    border.Top += yVelocity;
-                    border.Bottom -= yVelocity;
+                    this.Entity.Transform.Position.Y -= yVelocity;
                 }
                 if (Input.IsKeyDown(Keys.Up))
                 {
-                    border.Top -= yVelocity;
-                    border.Bottom += yVelocity;
+                    this.Entity.Transform.Position.Y += yVelocity;
                 }
+
+
             }
         }
     }
