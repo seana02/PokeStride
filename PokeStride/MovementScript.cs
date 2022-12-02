cc855b174eb38819e91c7bae59d93afa55fa2865
Sean A <seanseptember2002@gmail.com>
2022-12-02 17:50:52 -0500

    movement applied maybe

--- PokeStride/MovementScript.cs
+++ PokeStride/MovementScript.cs
@@ -7,6 +7,10 @@ using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
 using System.Runtime.CompilerServices;
+using Stride.Rendering.Sprites;
+using Stride.Graphics;
+using Stride.UI;
+using Stride.UI.Controls;
 
 namespace PokeStride
 {
@@ -14,11 +18,14 @@ namespace PokeStride
     {
         // Declared public member fields and properties will show in the game studio
         bool moveable;
-        float xVelocity, yVelocity;  
+        float xVelocity, yVelocity;
+        ImageElement sprite;
 
         public override void Start()
         {
             // Initialization of the script.
+            sprite = Entity.Get<UIComponent>().Page.RootElement.FindVisualChildOfType<ImageElement>("characterSprite");
+            Log.Info(sprite.ToString());
             xVelocity = 0.1f;
             yVelocity = 0.1f;
             moveable = true;
@@ -32,10 +39,12 @@ namespace PokeStride
                 if (Input.IsKeyDown(Keys.Left))
                 {
                     this.Entity.Transform.Position.X -= xVelocity;
+                    Log.Info("Moving Left: " + this.Entity.Transform.Position.X);
                 }
                 if (Input.IsKeyDown(Keys.Right))
                 {
                     this.Entity.Transform.Position.X += xVelocity;
+                    Log.Info("Moving Right: " + this.Entity.Transform.Position.X);
                 }
                 if (Input.IsKeyDown(Keys.Down))
                 {
