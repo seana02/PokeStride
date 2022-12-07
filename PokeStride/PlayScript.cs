29a6593041ac60296d9b74f573daa6c7e368001f
ezha0 <ezhao516@gmail.com>
2022-12-06 20:05:35 -0500

    working movement and page navigation!!!!!

--- PokeStride/PlayScript.cs
+++ PokeStride/PlayScript.cs
@@ -8,12 +8,15 @@ using Stride.Input;
 using Stride.Engine;
 using Stride.UI;
 using Stride.UI.Controls;
+using Stride.Core.Serialization;
 
 namespace PokeStride
 {
     public class PlayScript : SyncScript
     {
         // Declared public member fields and properties will show in the game studio
+        
+        public UrlReference<Scene> ChildSceneUrl { get; set; }
 
         public override void Start()
         {
@@ -21,7 +24,17 @@ namespace PokeStride
             UIPage page = Entity.Get<UIComponent>().Page;
             var rootElement = page.RootElement;
             var playButton = rootElement.FindVisualChildOfType<Button>("playButton");
-            playButton.Click += delegate { Entity.Get<UIComponent>().Page = Entity.Scene.Entities[5].Get<UIComponent>().Page; };
+            playButton.Click += delegate
+            {
+                rootElement.Visibility = Visibility.Hidden;
+
+
+                var parentScene = Entity.Scene;
+
+                var childScene = Content.Load(ChildSceneUrl);
+
+                parentScene.Children.Add(childScene);
+            };
         }
 
         public override void Update()
